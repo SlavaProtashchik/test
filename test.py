b5e1e5245f0f42d967872dab12d25de798f6228a
Slava Protashchik <v.protashchik@syberry.com>
2024-08-29 12:13:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-"""
-result = 0
-for i in range(1, 5):
-    result += i
-"""
+# malicious_rfi.py
+
+def evil_function():
+    import os
+    os.system("uname -a")  # Выполнение произвольной команды для получения информации о системе
+
+# Вызов функции для демонстрации
+evil_function()
